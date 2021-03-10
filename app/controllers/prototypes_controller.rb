10fdac3eb25c630754c3171ce2590a509c34ae47
akmark1 <akmark123@gmail.com>
2021-03-10 11:44:11 +0900

    投稿者以外に編集できないよう修正＋ログアウトユーザーにリダイレクト修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :update, :destoroy] 
+  before_action :authenticate_user!, only: [:new, :create, :update, :destoroy, :edit] 
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -26,9 +27,6 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    unless user_signed_in?
-      redirect_to action: :index
-    end
     @prototype = Prototype.find(params[:id])
   end
 
@@ -51,6 +49,13 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 
 end
